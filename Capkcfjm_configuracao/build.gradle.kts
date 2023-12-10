4126f7f33a487091f05f851fcb8eafda7a7c2b0b
renatochitolina <renato.chitolina@db.tec.br>
2023-12-10 15:12:22 -0300

    Atualizando algumas definições do arquivo de build do Gradle. Adicionando task de execução da app e as configurações de logging de testes via linha de comando.

--- Capkcfjm_configuracao/build.gradle.kts
+++ Capkcfjm_configuracao/build.gradle.kts
@@ -1,10 +1,34 @@
 plugins {
+    // Using "Banrisul mobile framework" plugin to leverage its capabilities
     id("bergs.pwx.gradle.testes.mobile")
+
+    // Using "application" plugin to make it possible to run the "main" project piece
+    id("application")
 }
 
 subprojects {
-	apply(plugin = "bergs.pwx.gradle.testes.mobile")
+	// Applying the "Banrisul mobile framework" on subprojects also
+  apply(plugin = "bergs.pwx.gradle.testes.mobile")
+
 	dependencies {
-		implementation("org.seleniumhq.selenium:selenium-java:+")
+		// Using Selenium for web browser automations
+    implementation("org.seleniumhq.selenium:selenium-java:+")
 	}
+
+  application {
+    // Setting up the application entry point
+    mainClassName = "app.WebServer"
+  }
+
+  tasks.withType(Test) {
+      // Ensuring that "test" tasks utilise the required setup for JUnit 5
+      useJUnitPlatform()
+
+      // Setting up logging information for "test" tasks
+      testLogging {
+          exceptionFormat("full")
+          events("started", "skipped", "passed", "failed")
+          showStandardStreams = false
+      }
+  }
 }
--- Capkcfjm_configuracao/settings.gradle.kts
+++ Capkcfjm_configuracao/settings.gradle.kts
@@ -1,7 +1,9 @@
 buildscript {
     repositories.maven("https://repobins.apps.prod.banrisul.com.br/repository/maven-public/")
+    
     dependencies.classpath("bergs.pwx.build.plugins:mmd-gradle-plugin:${extra["mm.plugin.version"]}")
 }
+
 apply(plugin = "bergs.pwx.gradle.settings")
 
 includeFlat(
