3289bdb176d54ed2ddbb56d4ace89fd3dc09e2ec
renatochitolina <renato.chitolina@db.tec.br>
2023-12-11 12:32:29 -0300

    Comentando blocos problemáticos dentro do ambiente Banri.

--- Capkcfjm_configuracao/build.gradle.kts
+++ Capkcfjm_configuracao/build.gradle.kts
@@ -15,20 +15,22 @@ subprojects {
     implementation("org.seleniumhq.selenium:selenium-java:+")
 	}
 
-  application {
-    // Setting up the application entry point
-    mainClassName = "app.WebServer"
-  }
+  // TODO: Revisar forma correta de aplicar dentro do ambiente Banrisul
+  // application {
+  //   // Setting up the application entry point
+  //   mainClassName = "app.WebServer"
+  // }
 
-  tasks.withType(Test) {
-      // Ensuring that "test" tasks utilise the required setup for JUnit 5
-      useJUnitPlatform()
+  // TODO: Revisar forma correta de aplicar dentro do ambiente Banrisul
+  // tasks.withType(Test) {
+  //     // Ensuring that "test" tasks utilise the required setup for JUnit 5
+  //     useJUnitPlatform()
 
-      // Setting up logging information for "test" tasks
-      testLogging {
-          exceptionFormat("full")
-          events("started", "skipped", "passed", "failed")
-          showStandardStreams = false
-      }
-  }
+  //     // Setting up logging information for "test" tasks
+  //     testLogging {
+  //         exceptionFormat("full")
+  //         events("started", "skipped", "passed", "failed")
+  //         showStandardStreams = false
+  //     }
+  // }
 }
